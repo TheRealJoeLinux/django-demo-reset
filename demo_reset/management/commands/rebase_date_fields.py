e721842b12318b74b517793728b5e51d23bdefbe
Joey Espinosa <jlouis.espinosa@gmail.com>
2013-04-16 14:09:10 -0400

    Cleaning up for PEP8 and modifying to use get_apps and get_models instead of ContentTypes

--- demo_reset/management/commands/rebase_date_fields.py
+++ demo_reset/management/commands/rebase_date_fields.py
@@ -1,43 +1,51 @@
+from dateutil import parser
+
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.contenttypes.models import ContentType
 from django.db.models import DateTimeField, DateField, F
-from django.core.exceptions import ValidationError
+from django.db.models.loading import get_apps, get_models
 from django.utils import timezone
 from django.conf import settings
+
 
 class Command(BaseCommand):
     args = "<rebase_date>"
     help = """
-    Rebases all date and datetime fields such that their offset from today is the
+    Rebases all date and datetime fields such that their offset from today will be the
     same as their offset from the specified <rebase_date>.
-    Requires d.c.contenttypes and Django>1.4.
+    Requires Django>1.4.
     """
 
     def handle(self, *args, **options):
         rebase_date = self.parse_rebase_date(args[0])
-        now = timezone.now().date()
-        delta = now - rebase_date
+        today = timezone.now().date()
+        delta = today - rebase_date
 
-        self.ignores = getattr(settings, 'DEMO_DATE_RESET_IGNORES', { })
-        
-        for model in ContentType.objects.all():
-            klass = model.model_class()
-            fields = self.get_date_fields_for_klass(klass)
-            if fields:
-                update_kwargs = dict([
-                    (field_name, F(field_name) + delta) for field_name in fields
-                    ])
-                klass.objects.update(**update_kwargs)
+        self.ignores = getattr(settings, 'DEMO_DATE_RESET_IGNORES', {})
+
+        for app in get_apps():
+            app_models = get_models(app)
+            if not app_models:
+                continue
+
+            app_name = app.__name__.split('.')[-2]
+            if app_name in self.ignores:
+                continue
+
+            for klass in app_models:
+                fields = self.get_date_fields_for_klass(klass)
+                if fields:
+                    update_kwargs = dict([(field_name, F(field_name) + delta) for field_name in fields])
+                    klass.objects.update(**update_kwargs)
 
     def parse_rebase_date(self, rebase_date_str):
         """
-        Parse the supplied rebase_date using a DateField
+        Parse the supplied rebase_date using "smart" dateutil.parser
         """
-        field = DateField()
         try:
-            return field.to_python(rebase_date_str)
-        except ValidationError:
-            raise CommandError('Cannot parse date from "%s"' % rebase_date_str)
+            parsed_date = parser.parse(rebase_date_str)
+        except ValueError:
+            raise CommandError('Cannot parse date from {0}'.format(rebase_date_str))
+        return parsed_date.date()
 
     def get_date_fields_for_klass(self, klass):
         """
@@ -45,13 +53,8 @@ class Command(BaseCommand):
         Returns a list of the field names to be updated
         """
         klass_name = '.'.join((klass.__module__, klass.__name__))
-        klass_ignores = self.ignores.get(klass_name, [ ])
-        return [
-                field.name
+        klass_ignores = self.ignores.get(klass_name, [])
+        return [field.name
                 for field in klass._meta.fields
-                if (
-                    isinstance(field, (DateTimeField, DateField))
-                    and field.name not in klass_ignores
-                )
-            ]
-
+                if (isinstance(field, (DateTimeField, DateField))
+                    and field.name not in klass_ignores)]
